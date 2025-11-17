6f71b3fcff324555f7c30f54041f2df5a99ae43d
Matt Herbst <m.herbst@my.ccsu.edu>
2025-11-17 00:53:05 -0500

    Had to recreate because not all files were generated

--- TicketMasterSite/TicketMasterSite/urls.py
+++ TicketMasterSite/TicketMasterSite/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for TicketFinder project.
+URL configuration for TicketMasterSite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
