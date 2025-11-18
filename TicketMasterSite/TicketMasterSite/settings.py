0ae9bdebd93b60a81a315e521b2b0b66a59ce388
mherbst61 <m.herbst@my.ccsu.edu>
2025-11-18 13:13:28 -0500

    From mac fix so it works here

--- TicketMasterSite/TicketMasterSite/settings.py
+++ TicketMasterSite/TicketMasterSite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'TicketFinder.apps.TicketfinderConfig',
-    'bootstrap5',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
