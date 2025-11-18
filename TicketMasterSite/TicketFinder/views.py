9e245c26f15ce6a40c1e67229b0f45b5a1c29f0d
mherbst61 <m.herbst@my.ccsu.edu>
2025-11-18 16:04:51 -0500

    edits made on how to do the add cards and messages and dealing with json.  Done on mac so issues may need to be transfered to windows.

--- TicketMasterSite/TicketFinder/views.py
+++ TicketMasterSite/TicketFinder/views.py
@@ -6,21 +6,37 @@ from TicketFinder.forms import ticketSearchForm
 
 
 # Create your views here.
-def Search(request):
+def search(request):
 
-    form = ticketSearchForm(request.GET or None)
-
-
-    if(request.method == "GET"):
-        if(form.is_valid()):
-
+    form = ticketSearchForm(request.POST or None)
+    message = ""
+    eventObject = []
+    if request.method == "POST":
+        if form.is_valid():
             apiKey = "Bd9NVuUJf6tdtp7GETrrQvkuMtOVm4fk"
             city = form.cleaned_data['city']
             genre = form.cleaned_data['genre']
+            print(city, genre)
             try:
                 response = requests.get("https://app.ticketmaster.com/discovery/v2/events.json?&sort=date,asc&countryCode=US&city=" + city + "&classificationName=" + genre + "&apikey=" + apiKey)
-                print(response.json())
+                data = response.json()
+                #print(data)
+                for event in data["_embedded"] ["events"]:
+                    event_name = event["name"]
+                    event_url = event["url"]
+                    print(event_name)
+                    ticket = {
+                        "event_name": event_name,
+                        "event_url": event_url,
+                    }
+                    eventObject.append(ticket)
+
             except:
                 print("No results found")
-    context = {'form': form}
+                message = "No results found"
+    context = {
+        'form': form,
+        'tickets': eventObject,
+        'message' : message
+    }
     return render(request,'ticketmasterhtml.html', context)
