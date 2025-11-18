8dcf95eb153d08a2061a87f16d82719a94efede6
mherbst61 <m.herbst@my.ccsu.edu>
2025-11-18 13:41:13 -0500

    Added alert message, small change in ticketmasterhtml. try catch in views. Done on mac so some mac files might be here

--- TicketMasterSite/TicketFinder/views.py
+++ TicketMasterSite/TicketFinder/views.py
@@ -9,13 +9,18 @@ from TicketFinder.forms import ticketSearchForm
 def Search(request):
 
     form = ticketSearchForm(request.GET or None)
+
+
     if(request.method == "GET"):
         if(form.is_valid()):
+
             apiKey = "Bd9NVuUJf6tdtp7GETrrQvkuMtOVm4fk"
             city = form.cleaned_data['city']
             genre = form.cleaned_data['genre']
-            response = requests.get("https://app.ticketmaster.com/discovery/v2/events.json?&sort=date,asc&countryCode=US&city=" + city + "&classificationName=" + genre + "&apikey=" + apiKey)
-            print(response.json())
-
+            try:
+                response = requests.get("https://app.ticketmaster.com/discovery/v2/events.json?&sort=date,asc&countryCode=US&city=" + city + "&classificationName=" + genre + "&apikey=" + apiKey)
+                print(response.json())
+            except:
+                print("No results found")
     context = {'form': form}
     return render(request,'ticketmasterhtml.html', context)
